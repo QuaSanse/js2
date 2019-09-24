07871b3a077b04084991f9c22be60f42a4c3971a
WS01 <quasanse@gmail.com>
2019-09-24 21:20:02 +0300

    js2

---
+++ unit_1/js/script.js
@@ -0,0 +1,78 @@
+// Unit 1
+
+// Task 1
+console.log('Dima');
+
+// Task 2
+console.log(9);
+
+// Task 3
+console.log('Добро ' + 'пожаловать' + ' на курс');
+let task_3 = ('Добро ' + 'пожаловать' + ' на курс'); //Через переменную
+console.log(task_3);
+
+// Task 4
+//alert(2019);
+
+// Task 5
+//alert(2019 - 200);
+//let task_5 = (2019 - 200); //Через переменную
+//alert(task_5);
+
+// Task 6
+document.getElementById('one').innerHTML = 'Hello World!';
+
+// Task 7
+document.getElementById('two').innerHTML = 12 * 12;
+
+//Task 8
+
+
+// Task 9
+document.querySelector('span').innerHTML = 'World';
+
+// Task 10
+document.querySelector('.three').innerHTML = '<h3>Text h3</>';
+
+// Task 11
+document.querySelector('.four').innerHTML += '<h3>Text four =1</> <p> And text par =1</>';
+document.querySelector('.four').innerHTML += '<h3>Text four =2</>' + '<p> And text par =2</>';
+
+// Task 12
+//let a = document.querySelector('.five').innerHTML = 3.1415;
+let a = document.querySelector('.five')
+a.innerHTML = 3.1415;
+
+// Task 13
+let div7 = document.querySelector('.seven');
+div7.innerHTML = '<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">';
+
+// Task 14
+let z1 = 6;
+let z2 = 3;
+document.getElementById('Task_14').innerHTML = z1 * z2;
+
+// Task 15
+let y1 = 6;
+let y2 = 3;
+document.getElementById('Task_15').innerHTML = y1 / y2;
+
+// Task 16
+let x1 = 'Hello'
+let x2 = 5
+document.getElementById('Task_16').innerHTML = x1 + x2;
+
+// Task 17
+let d1 = document.querySelector('.test-1');
+console.log(d1);
+
+// Task 18
+let d2 = document.querySelector('.test-2');
+console.log(d2 = 5);
+
+// Task 19
+let divS3 = document.querySelector('.s3');
+console.log(divS3 = document.querySelector('.s4'));
+
+// Task 20
+//document.querySelector('body')...innerHTML = '';
---
+++ unit_2/js/script.js
@@ -0,0 +1,70 @@
+// Unit 2
+
+// Task 1
+let a = 7;
+let b = 9;
+console.log(a * b);
+
+// Task 2
+let c = 7;
+let d = 9;
+console.log(c / d);
+
+// Task 3
+let e = 3;
+let f = 5;
+console.log(e + f);
+
+// Task 4
+let e1 = '3';
+let f1 = 5;
+console.log(e1 + f1); // Значение переменной e1 принимается как строковое,
+// поэтому происходит конкотенация
+
+// Task 5
+let e2 = 3;
+let f2 = 0;
+console.log(e2 / f2);
+
+// Task 6
+let e3 = 3;
+let f3 = 'Hello';
+console.log(e3 + f3);
+
+// Task 7
+let e4 = 3;
+let f4 = 'Hello';
+console.log(e4 * f4);
+
+// Task 8
+let inputIn_1 = document.querySelector('.input_in1');
+let button_1 = document.querySelector('.button_1');
+
+button_1.onclick = function () {
+    console.log(inputIn_1.value);
+}
+
+// Task 9
+let inputIn_2 = document.querySelector('.input_in2');
+let button_2 = document.querySelector('.button_2');
+let aut_2 = document.querySelector('.aut_2');
+
+button_2.onclick = function () {
+    let vl_2 = inputIn_2.value;
+    //console.log(vl_2); вывод в консоль
+    aut_2.innerHTML = vl_2; // вывод на страницу
+    inputIn_2.value = ''; // очистка поля после вывода
+}
+
+// Task 10
+let input_t10 = document.querySelector('.input_t10'); // получаем input
+let button_t10 = document.querySelector('.button_t10'); // получаем button
+let aut_10 = document.querySelector('.aut_10'); // получаем место для вывода значения
+
+button_t10.onclick = function () {
+    let vl_10 = +input_t10.value; // получаем введенное значение
+    aut_10.innerHTML = vl_10 * 10; // присваиваем значение
+    input_t10.value = ''; // очистка поля после вывода
+}
+
+// Task 11
