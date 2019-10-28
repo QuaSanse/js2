836592059156ef3366344044903e6c709b4919cf
WS01 <quasanse@gmail.com>
2019-10-28 21:23:47 +0300

    Unit 5, task 7 - 11

--- unit_5/js/script.js
+++ unit_5/js/script.js
@@ -37,3 +37,70 @@ let aut_t6 = '';
 for (let i = 0; i <= 5; i++) {
     aut_t6 += '*************' + '<br>';
 } sp_t6.innerHTML = aut_t6;
+
+//Task 7 --------------------
+let inp_t7 = document.querySelector('.inp_t7');
+let btn_t7 = document.querySelector('.btn_t7');
+let sp_t7 = document.querySelector('.sp_t7');
+
+let aut_t7 = '';
+btn_t7.onclick = () => {
+    let vl_t7 = +inp_t7.value;
+    for (let i = vl_t7; i >= 0; i--) {
+        aut_t7 += i + ' ';
+    } sp_t7.innerHTML = aut_t7;
+}
+
+//Task 8 --------------------
+let inp_t8_1 = document.querySelector('.inp_t8_1');
+let inp_t8_2 = document.querySelector('.inp_t8_2');
+let btn_t8 = document.querySelector('.btn_t8');
+let sp_t8 = document.querySelector('.sp_t8');
+
+let aut_t8 = '';
+btn_t8.onclick = () => {
+    let vl_t8_1 = +inp_t8_1.value;
+    let vl_t8_2 = +inp_t8_2.value;
+
+    for (let i = vl_t8_1; i <= vl_t8_2; i++) {
+        aut_t8 += i + ' ';
+    } sp_t8.innerHTML = aut_t8;
+}
+
+//Task 9 --------------------
+let inp_t9_1 = document.querySelector('.inp_t9_1');
+let inp_t9_2 = document.querySelector('.inp_t9_2');
+let btn_t9 = document.querySelector('.btn_t9');
+let sp_t9 = document.querySelector('.sp_t9');
+
+let aut_t9 = '';
+btn_t9.onclick = () => {
+    let vl_t9_1 = +inp_t9_1.value;
+    let vl_t9_2 = +inp_t9_2.value;
+
+    if (vl_t9_2 > vl_t9_1) {
+        for (let i = vl_t9_1; i <= vl_t9_2; i++) {
+            aut_t9 += i + ' ';
+        } sp_t9.innerHTML = aut_t9;
+    } else {
+        alert('Ошибка, первое число больше второго!');
+    }
+}
+
+//Task 10 --------------------
+let sp_t10 = document.querySelector('.sp_t10');
+
+let aut_t10 = '';
+for (let i = 1901; i <= 1950; i++) {
+    if (i % 2 == 0) {
+        aut_t10 += i + ' ';
+    }
+} sp_t10.innerHTML = aut_t10;
+
+//Task 11 --------------------
+let div = document.querySelectorAll('.one');
+let sp_t11 = document.querySelector('.sp_t11');
+
+for (let i = 0; i <= div.length; i++) {
+    sp_t11.innerHTML = i;
+}
