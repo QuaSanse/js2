7407dc7d840aa82ea9a3eb76b101094dfb960564
WS01 <quasanse@gmail.com>
2019-11-03 20:35:27 +0300

    Unit 6, task 1-4

--- unit_6/js/script.js
+++ unit_6/js/script.js
@@ -1,1 +1,43 @@
 // Task 1 --------------------
+let sp_t1 = document.getElementById('sp_t1');
+
+for (let i = 0; i < 3; i++) {
+    for (let k = 0; k < 3; k++) {
+        sp_t1.innerHTML += '*';
+    }
+    sp_t1.innerHTML += '_';
+}
+
+// Task 2 --------------------
+let sp_t2 = document.getElementById('sp_t2');
+
+for (let i = 1; i < 4; i++) {
+    sp_t2.innerHTML += i + '<br>';
+    for (let k = 0; k < 3; k++) {
+        sp_t2.innerHTML += '*_'
+    }
+    sp_t2.innerHTML += '<br>'
+}
+
+// Task 3 --------------------
+let sp_t3 = document.getElementById('sp_t3');
+
+for (let i = 0; i < 4; i++) {
+    for (let k = 0; k < 3; k++) {
+        sp_t3.innerHTML += '*_';
+    }
+    sp_t3.innerHTML += '<br>';
+}
+
+// Task 4 --------------------
+let sp_t4 = document.getElementById('sp_t4');
+
+let aut_t4 = '';
+for (let i = 0; i <= 10; i++) {
+    // 1
+    aut_t4 += i + '_';
+    for (let k = 0; k < 1; k++) {
+        // 1
+        aut_t4 += 10 - i + '_';
+    }
+} sp_t4.innerHTML = aut_t4;
