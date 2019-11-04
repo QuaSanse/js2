b405006cf2e8be002c417c0ddc8923fbea33d722
WS01 <quasanse@gmail.com>
2019-11-04 22:03:55 +0300

    Unit 6, task 5-8

--- unit_6/js/script.js
+++ unit_6/js/script.js
@@ -41,3 +41,68 @@ for (let i = 0; i <= 10; i++) {
         aut_t4 += 10 - i + '_';
     }
 } sp_t4.innerHTML = aut_t4;
+
+// Task 5 --------------------
+let sp_t5 = document.getElementById('sp_t5');
+
+aut_t5 = '';
+for (let i = 0; i < 3; i++) {
+    if (i != 0) {
+        aut_t5 += '<br>';
+    }
+    for (let k = 0; k < 6; k++) {
+        if (k % 2 == 0) {
+            aut_t5 += '1';
+        } else {
+            aut_t5 += '0';
+        }
+    }
+} sp_t5.innerHTML = aut_t5;
+
+// Task 6 --------------------
+let sp_t6 = document.getElementById('sp_t6');
+
+aut_t6 = '';
+for (let i = 0; i < 3; i++) {
+    if (i != 0) {
+        aut_t6 += '<br>';
+    }
+    for (let k = 0; k < 6; k++) {
+        switch (true) {
+            case k == 2 || k == 5:
+                aut_t6 += 'x';
+                break;
+            case (k % 2 == 0):
+                aut_t6 += '1';
+                break;
+            default:
+                aut_t6 += '0';
+        }
+    }
+} sp_t6.innerHTML = aut_t6;
+
+// Task 7 --------------------
+let sp_t7 = document.getElementById('sp_t7');
+
+aut_t7 = '';
+for (let i = 0; i < 4; i++) {
+    if (i != 0) {
+        aut_t7 += '<br>';
+    }
+    for (let k = 0; k <= i; k++) {
+        aut_t7 += '*';
+    }
+} sp_t7.innerHTML = aut_t7;
+
+// Task 8 --------------------
+let sp_t8 = document.getElementById('sp_t8');
+
+aut_t8 = '';
+for (let i = 5; i >= 0; i--) {
+    if (i != 5) {
+        aut_t8 += '<br>';
+    }
+    for (let k = i; k > 0; k--) {
+        aut_t8 += '*';
+    }
+} sp_t8.innerHTML = aut_t8;
