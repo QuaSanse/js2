5ad1095f8f65e7806f7c1f603177a9dbe9df44ba
WS01 <quasanse@gmail.com>
2019-11-05 19:07:07 +0300

    Unit 6, task 9 - 10 and challenge 1-3

--- unit_6/js/script.js
+++ unit_6/js/script.js
@@ -106,3 +106,83 @@ for (let i = 5; i >= 0; i--) {
         aut_t8 += '*';
     }
 } sp_t8.innerHTML = aut_t8;
+
+// Task 9 --------------------
+let sp_t9 = document.getElementById('sp_t9');
+
+aut_t9 = '';
+for (let i = 1; i <= 5; i++) {
+    if (i != 1) {
+        aut_t9 += '<br>';
+    }
+    for (let k = 1; k <= i; k++) {
+        aut_t9 += k + ' ';
+    }
+} sp_t9.innerHTML = aut_t9;
+
+// Task 10 --------------------
+let sp_t10 = document.getElementById('sp_t10');
+
+aut_t10 = '';
+for (let i = 0; i < 5; i++) {
+    if (i != 0) {
+        aut_t10 += '<br>';
+    }
+    for (let k = 1; k <= 10; k++) {
+        if ((i * 10 + k) < 10) {
+            aut_t10 += '0' + (i * 10 + k) + ' ';
+        } else {
+            aut_t10 += (i * 10 + k) + ' ';
+        }
+    }
+} sp_t10.innerHTML = aut_t10;
+
+// Задачи на прокачку
+// Task 1 --------------------
+let sp_pt1 = document.getElementById('sp_pt1');
+
+aut_pt1 = '';
+for (let i = 0; i < 3; i++) {
+    aut_pt1 += '<br> &nbsp;&nbsp;&nbsp;';
+
+    for (let k = 0; k < 5; k++) {
+        aut_pt1 += '*';
+    }
+} sp_pt1.innerHTML = aut_pt1;
+
+// Task 2 --------------------
+let sp_pt2 = document.getElementById('sp_pt2');
+
+aut_pt2 = '';
+for (let i = 0; i < 3; i++) {
+    aut_pt2 += '<br> &nbsp;&nbsp;&nbsp;';
+
+    for (let k = 0; k < 6; k++) {
+        if (k % 2 == 0) {
+            aut_pt2 += '1';
+        } else {
+            aut_pt2 += '0';
+        }
+    }
+} sp_pt2.innerHTML = aut_pt2;
+
+// Task 3 --------------------
+let sp_pt3 = document.getElementById('sp_pt3');
+
+aut_pt3 = '';
+for (let i = 0; i < 3; i++) {
+    aut_pt3 += '<br> &nbsp;&nbsp;&nbsp;';
+
+    for (let k = 0; k < 6; k++) {
+        if (k == 2 || k == 5) {
+            aut_pt3 += 'x';
+            continue;
+        }
+        if (k % 2 == 0) {
+            aut_pt3 += '1';
+            continue;
+        } else {
+            aut_pt3 += '0';
+        }
+    }
+} sp_pt3.innerHTML = aut_pt3;
