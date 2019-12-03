66f476e7573acc1502e6f29c2a66c6c6cd57b971
WS01 <quasanse@gmail.com>
2019-12-03 21:47:49 +0300

    Unit 08 Challange, tasks 5 - 7

--- unit_8_challenge/unit_08.js
+++ unit_8_challenge/unit_08.js
@@ -1,7 +1,4 @@
 //  Task 1
-// Кнопка b-1 запускает функцию t1. Функция должна выводить в out-1 строку вида:
-//     1 2 3 4 5 6 7 8 9 ... 49 50
-// от 1 до 50 включительно. Разделитель - пробел. Задача решается с помощью цикла.
 
 let out_t1 = '';
 function t1() {
@@ -17,9 +14,6 @@ function t1() {
 document.querySelector('.b-1').onclick = t1;
 
 //  Task 2
-// Кнопка b-2 запускает функцию t2. Функция должна выводить в out-2 строку вида:
-//     2 4 6 ... 122
-// от 2 до 122 c шагом 2. Разделитель - пробел. Задача решается с помощью цикла  while.
 
 let out_t2 = '';
 function t2() {
@@ -37,9 +31,6 @@ document.querySelector('.b-2').onclick = t2;
 
 
 //  Task 3
-// Кнопка b-3 запускает функцию t3. Функция должна выводить в out-3 строку вида:
-//     25 24 23 22 . . 7
-// от 25 до 7 c шагом 1. Разделитель - пробел. Задача решается с помощью цикла  while.
 
 out_t3 = '';
 function t3() {
@@ -65,9 +56,6 @@ function t3() {
 document.querySelector('.b-3').onclick = t3;
 
 //  Task 4
-// Кнопка b-4 запускает функцию t4. Функция должна выводить в out-4 строку вида:
-//     77_74_71_68_65_62_ ... _38_35_
-// от 77 до 35 c шагом 3. Разделитель - знак подчеркивания. Задача решается с помощью цикла  while.
 
 out_t4 = '';
 function t4() {
@@ -95,16 +83,12 @@ document.querySelector('.b-4').onclick = t4;
 
 
 //  Task 5
-// Кнопка b-5 запускает функцию t5. Функция должна выводить в out-5 строку вида:
-//     1_*2_**3_*4_** ... 17_*
-// от 1 до 17 c шагом 1. Разделитель - знак подчеркивания и звездочка (если число нечетное, и две звездочки если четное). Задача решается с помощью цикла  while.
 
-out_t5 = '';
+out_t5 = 0;
 function t5() {
-    let i = 1;
-    while (i <= 17) {
-        if (i % 2 == 0) { out_t5 += i + '_**' }
-        else { out_t5 += i + '_*' }
+    let i = 0;
+    while (i <= 20) {
+        out_t5 += i;
         i++;
     }
     document.querySelector('.out-5').textContent = out_t5;
@@ -114,26 +98,21 @@ document.querySelector('.b-5').onclick = t5;
 
 
 //  Task 6
-// Кнопка b-6 запускает функцию t6. Функция должна выводить в out-6 строку вида:
-// ******
-// ******
-// ******
-// Задача решается с помощью цикла. В каждой итерации цикл выводит 6 звездочек без пробелов и символ переноса строки br.
-// Количество строк (итераций, повторений) цикла  while вводит пользователь в i-6.
 
 out_t6 = '';
 function t6() {
-    let it = 0;
-    let vl_6 = document.querySelector('.i-6').value;
-    while (it < vl_6) {
-        let str = 0;
-        while (str < 6) {
-            out_t6 += '*';
-            str++;
+    let vl_6_1 = +document.querySelector('.i-p6_1').value;
+    let vl_6_2 = +document.querySelector('.i-p6_2').value;
+
+    if (vl_6_1 < vl_6_2) {
+        let sum = 0;
+
+        while (vl_6_1 <= vl_6_2) {
+            sum += vl_6_1;
+            vl_6_1++;
         }
-        out_t6 += ' ';
-        it++;
-    }
+        out_t6 = sum;
+    } else { out_t6 = false }
     document.querySelector('.out-6').innerHTML = out_t6;
 }
 
@@ -141,21 +120,21 @@ document.querySelector('.b-6').onclick = t6;
 
 
 //  Task 7
-// Есть input i-7 куда пользователь может ввести число больше нуля (проверок не делаем, принимаем как факт).
-// По нажатию кнопки b-7 должна запускаться функция f7, которая выводит в out-7 числа от введенного пользователем до нуля включительно.
-// Разделитель пробел. Если пользователь ввел 4 и нажал кнопку, мы получим:
-//     4 3 2 1
-// Задача решается с помощью цикла  while.
 
 out_t7 = '';
 function t7() {
-    let vl_t7 = document.querySelector('.i-7').value;
-    while (vl_t7 >= 0) {
+    let vl_7_1 = +document.querySelector('.i-p7_1').value;
+    let vl_7_2 = +document.querySelector('.i-p7_2').value;
 
-        out_t7 += vl_t7 + ' ';
-        vl_t7--;
-    }
-    document.querySelector('.out-7').textContent = out_t7;
+    if (vl_7_1 < vl_7_2) {
+        mult = 1;
+        while (vl_7_1 <= vl_7_2) {
+            mult = mult * vl_7_1;
+            vl_7_1++;
+        }
+        out_t7 = mult;
+    } else { out_t7 = false }
+    document.querySelector('.out-7').innerHTML = out_t7;
 }
 
 document.querySelector('.b-7').onclick = t7;
